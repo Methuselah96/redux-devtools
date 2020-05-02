0ef76a9c7ceb0a193f92764c3334cdba92fab594
Nathan Bierema <nbierema@gmail.com>
2020-05-02 00:14:07 -0400

    Get stuff working

---
+++ packages/react-json-tree/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  }
+};
