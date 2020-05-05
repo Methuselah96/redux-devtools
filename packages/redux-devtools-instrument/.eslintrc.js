9bedabf3ac940a380a05c4b4f69fb7ee2b1f0a9b
Nathan Bierema <nbierema@gmail.com>
2020-05-05 12:15:29 -0400

    Setup

---
+++ packages/redux-devtools-instrument/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
