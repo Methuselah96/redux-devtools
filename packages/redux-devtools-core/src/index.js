5335809b4a8d33f60ee0a35fe568bc09d1bace2c
Nathan Bierema <nbierema@gmail.com>
2020-05-07 01:03:36 -0400

    Move zalmoxisus/remotedev-app

--- packages/redux-devtools-core/src/index.js
+++ packages/redux-devtools-core/src/index.js
@@ -1,25 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './app';
-
-render(
-  <App />,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  // https://github.com/webpack/webpack/issues/418#issuecomment-53398056
-  module.hot.accept(err => {
-    if (err) console.error(err.message);
-  });
-
-  /*
-    module.hot.accept('./app', () => {
-      const NextApp = require('./app').default;
-      render(
-        <NextApp />,
-        document.getElementById('root')
-      );
-    });
-   */
+function injectedScript() {
+  console.error('Not implemented yet. WIP. If you\'re looking for utils, ' +
+    'import `redux-devtools-core/lib/utils`.');
 }
+
+export default injectedScript;
