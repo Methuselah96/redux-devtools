0ab560b932100d1e57c73c5440168d255137b4f5
Nathan Bierema <nbierema@gmail.com>
2020-05-11 23:06:51 -0400

    No need for react

--- packages/redux-devtools-instrument/.eslintrc.js
+++ packages/redux-devtools-instrument/.eslintrc.js
@@ -2,16 +2,16 @@ module.exports = {
   extends: '../../.eslintrc',
   overrides: [
     {
-      files: ['*.ts', '*.tsx'],
-      extends: '../../eslintrc.ts.react.base.json',
+      files: ['*.ts'],
+      extends: '../../eslintrc.ts.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
         project: ['./tsconfig.json']
       }
     },
     {
-      files: ['test/*.ts', 'test/*.tsx'],
-      extends: '../../eslintrc.ts.react.jest.base.json',
+      files: ['test/*.ts'],
+      extends: '../../eslintrc.ts.jest.base.json',
       parserOptions: {
         tsconfigRootDir: __dirname,
         project: ['./test/tsconfig.json']
